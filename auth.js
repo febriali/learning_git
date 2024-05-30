402f99706eb9e15090b7e3d67bc0ee01c115a602
Febri Ali <febriali.metropolis@gmail.com>
2024-05-30 10:57:09 +0700

    fixed authentication wording

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function authenticate(login, password) {
   if (login === 'login' && password === 'password') {
-    return 'You are login';
+    return 'You are login!';
   } else {
     return 'Login is not correct';
   }
