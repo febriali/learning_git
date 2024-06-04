ea17c6f5dde27dcb5046e9bc045d0de2af625e6e
Febri Ali <febriali.metropolis@gmail.com>
2024-06-04 08:49:12 +0700

    added auth

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function authenticate(login, password) {
   if (login === 'login' && password === 'password') {
-    return 'You are login!';
+    return 'Welcome';
   } else {
     return 'Login is not correct';
   }
